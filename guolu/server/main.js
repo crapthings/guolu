b5849b0058a2b8865cafc7b42bb1975311d8fe15
crapthings <crapthings@gmail.com>
2017-10-31 22:08:26 +0800

    close #2

--- guolu/server/main.js
+++ guolu/server/main.js
@@ -1,23 +1,34 @@
-var mosca = require('mosca')
+import mosca from 'mosca'
 
-var settings = {
+const broker = new mosca.Server({
   port: 1883,
-}
+})
 
-var server = new mosca.Server(settings)
-
-server.on('clientConnected', function(client) {
-    console.log('client connected', client.id)
+broker.on('clientConnected', function (client) {
+  console.log('client connected', client.id)
 })
 
 // fired when a message is received
-server.on('published', function(packet, client) {
+broker.on('published', function (packet, client) {
   console.log('Published', packet.payload)
 })
 
-server.on('ready', setup)
+broker.on('ready', setup)
 
-// fired when the mqtt server is ready
 function setup() {
-  console.log('Mosca server is up and running')
+  console.log('broker is up and running')
 }
+
+
+// observe
+
+// var message = {
+//   topic: '/hello/world',
+//   payload: 'abcde', // or a Buffer
+//   qos: 0, // 0, 1, or 2
+//   retain: false // or true
+// };
+
+// server.publish(message, function() {
+//   console.log('done!');
+// });
